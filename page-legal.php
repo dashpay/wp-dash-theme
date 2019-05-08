71774227f1c4b7b2b90430be09b5ea5db5bdd74d
strophy <leon.white@dash.org>
2019-05-08 18:37:30 +0800

    Add new template for legal pages
    Based on default page.php

---
+++ page-legal.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+
+	<main role="main" class="no-banner">
+		<!-- section -->
+		<section class="container">
+
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<div class="richtext" style="padding-top: 2em;">
+					<?php the_content(); ?>
+				</div>
+				
+				<?php comments_template( '', true ); // Remove if you don't want comments ?>
+
+				<br class="clear">
+
+				<?php edit_post_link(); ?>
+
+			</article>
+			<!-- /article -->
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
