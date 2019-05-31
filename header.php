904c988de9e532d35c7275d454e9b83b7092b78f
strophy <leon.white@dash.org>
2019-05-31 13:17:37 +0800

    Don't push tagline to meta description

--- header.php
+++ header.php
@@ -17,7 +17,6 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,500,700,900i|Open+Sans:400,600,700|Roboto+Condensed" rel="stylesheet">
 
