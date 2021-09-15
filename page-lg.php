decc878a36348468c32566ca63393f515e11f699
saratranberg <85379892+saratranberg@users.noreply.github.com>
2021-09-15 16:11:47 -0600

    feat: dashdirect feature and button on homepage (#106)

--- page-lg.php
+++ page-lg.php
@@ -19,6 +19,15 @@ get_header(); ?>
 			<div class="caption">
 				<h1><?php echo get_field( "large_banner_title" ); ?></h1>
 				<p><?php echo get_field( "large_banner_subtitle" ); ?></p>
+				
+				<?php if ( get_field( "large_banner_button" )!=''){
+					?>
+					<br><a href="<?php echo get_field( "large_banner_button_link" )?>" 
+					class="banner-btn btn btn-blue"
+					target="<?php if ( get_field( "large_banner_button_new_tab" ) ) { echo "_blank"; }?>">
+					<?php echo get_field( "large_banner_button" )?></a>
+				<?php
+				}; ?>
 
 				<?php if ( get_field( "large_banner_video" )!='' ){
 					?>
