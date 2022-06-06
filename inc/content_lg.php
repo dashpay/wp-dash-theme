001666d7c20ec0fae98bd68ec804df9b531a0b41
Sara Tranberg <sara.tranberg@dash.org>
2022-06-05 22:32:19 -0600

    feat: title and subtitle on text section

--- inc/content_lg.php
+++ inc/content_lg.php
@@ -66,6 +66,15 @@ while( have_rows('content_sections') ): the_row();
 
 
 		<?php if ($type=='text'){ ?>
+			<div class="text-section-header-wrap">
+				<?php if ( get_sub_field ( "section_title")!=''){ ?>
+				<h3><strong><?php echo get_sub_field('section_title'); ?></strong></h3>
+				<?php } ?>
+
+				<?php if ( get_sub_field ( "section_subheading")!=''){ ?>
+				<h4><?php echo get_sub_field('section_subheading'); ?></h4>
+				<?php } ?>
+			</div>	
 			<div class="richtext text-lg-center">
 				<?php echo get_sub_field('section_description') ?>
 			</div>
