27cae87e168372997aa981a55fc18ee001f6e5e6
Silvanas <galaxywebsapce@gmail.com>
2022-07-21 15:03:36 +0200

    feat:social share buttons in posts

---
+++ inc/linkedin.svg.php
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 23.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 240 240" style="enable-background:new 0 0 240 240;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#FFFFFF;}
+</style>
+<path class="st0" d="M205.2,20H34.8C26.6,20,20,26.5,20,34.4v171.2c0,8,6.6,14.4,14.8,14.4h170.4c8.2,0,14.8-6.5,14.8-14.4V34.4
+	C220,26.5,213.4,20,205.2,20z M79.3,190.4H49.7V95h29.7L79.3,190.4L79.3,190.4z M64.5,81.9c-9.5,0-17.2-7.7-17.2-17.2
+	S55,47.5,64.5,47.5s17.2,7.7,17.2,17.2S74,81.9,64.5,81.9z M190.4,190.4h-29.6V144c0-11.1-0.2-25.3-15.4-25.3
+	c-15.4,0-17.8,12.1-17.8,24.5v47.2H97.9V95h28.4v13h0.4c4-7.5,13.6-15.4,28.1-15.4c30,0,35.6,19.8,35.6,45.5V190.4z"/>
+</svg>
