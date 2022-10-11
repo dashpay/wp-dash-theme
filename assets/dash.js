c0f234833096fa7bbb506a640271a48ccd53faf2
Silvanas <galaxywebsapce@gmail.com>
2022-10-11 10:06:46 +0200

    fix:menu closed in desktop

--- assets/dash.js
+++ assets/dash.js
@@ -267,6 +267,7 @@ $(window).resize(function(){
 })
 
 // Close menu when out of div 
+if(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)){
 $(document).mouseup(function(e){
    var header = $ (".header, .lang-mobile");
    var menu = $(".navbar, .dropdown");
@@ -277,7 +278,7 @@ $(document).mouseup(function(e){
 	  menu.hide(); 
    }
 });
-
+}
 //Links for Tab
 jQuery(function($){
   var hash = window.location.hash;
