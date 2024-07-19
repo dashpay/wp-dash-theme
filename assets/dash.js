d6c0850c56bf71a9a87ca08a457b78d426354edb
Silvanasss <gothicdark.love@gmail.com>
2024-07-19 14:57:58 -0300

    fix:day-time-fix

--- assets/dash.js
+++ assets/dash.js
@@ -295,14 +295,14 @@ jQuery(function($){
 });
 
 $(document).ready(function() {
-    // Set the date we're counting down to
-    var countDownDate = new Date("Jul 29, 2024 00:00:00").getTime();
+    // Set the date we're counting down to in ET
+    var countDownDate = new Date("Jul 29, 2024 00:00:00 GMT-0400").getTime(); // ET is currently UTC-4 during Daylight Saving Time
 
     // Update the count down every 1 second
     var x = setInterval(function() {
-
-        // Get today's date and time
-        var now = new Date().getTime();
+        // Get today's date and time in ET
+        var now = new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
+        now = new Date(now).getTime();
 
         // Find the distance between now and the count down date
         var distance = countDownDate - now;
@@ -326,6 +326,7 @@ $(document).ready(function() {
         }
     }, 1000);
 });
+
 
 /* NEWSLETTER FORM SCRIPT SENDGRID 
 const myForm = document.getElementById('dash-nl');
