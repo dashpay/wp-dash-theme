30652059608c9f817ce829c887c0d8bd8993fdc2
Silvanasss <gothicdark.love@gmail.com>
2024-07-19 15:00:07 -0300

    fix:day-time-fix

--- assets/dash.js
+++ assets/dash.js
@@ -294,38 +294,7 @@ jQuery(function($){
   });
 });
 
-$(document).ready(function() {
-    // Set the date we're counting down to
-    var countDownDate = new Date("Jul 29, 2024 00:00:00").getTime();
 
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-        // Get today's date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Output the result in an element with id="demo"
-        $('.days').text(days);
-        $('.hours').text(hours.toString().padStart(2, '0'));
-        $('.minutes').text(minutes.toString().padStart(2, '0'));
-        $('.seconds').text(seconds.toString().padStart(2, '0'));
-
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            $('.countdown').html("EXPIRED");
-        }
-    }, 1000);
-});
 
 /* NEWSLETTER FORM SCRIPT SENDGRID 
 const myForm = document.getElementById('dash-nl');
