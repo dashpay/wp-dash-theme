e377371bbed75da361ef40049cd12dba2591fe13
Silvanasss <gothicdark.love@gmail.com>
2024-09-18 22:51:38 -0300

    fix:header-expire-fix

--- assets/dash.js
+++ assets/dash.js
@@ -304,40 +304,6 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
-    // Set the date we're counting down to in ET
-    var countDownDate = new Date("Jul 29, 2024 11:00:00 GMT-0400").getTime(); // ET is currently UTC-4 during Daylight Saving Time
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-        // Get today's date and time in ET
-        var now = new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
-        now = new Date(now).getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Output the result in an element with id="demo"
-        $('.days').text(days);
-        $('.hours').text(hours.toString().padStart(2, '0'));
-        $('.minutes').text(minutes.toString().padStart(2, '0'));
-        $('.seconds').text(seconds.toString().padStart(2, '0'));
-
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            $('.countdown').html("EXPIRED");
-        }
-    }, 1000);
-});
-
-
 /* NEWSLETTER FORM SCRIPT SENDGRID 
 const myForm = document.getElementById('dash-nl');
 	const alertContainer = document.getElementById('warningDiv')
