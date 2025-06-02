89cc477df259164375ba9eba2e05527b2148ea39
Silvanasss <gothicdark.love@gmail.com>
2025-06-02 14:53:53 -0300

    feat:big-homepage-and-footer-update

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 							<a href="https://github.com/dashpay"><div class="icon-el github"></div></a>
 							<a href="https://discordapp.com/invite/PXbUxJB"><div class="icon-el discord"></div></a>
 							<a href="https://t.me/dash_chat"><div class="icon-el telegram"></div></a>
-							<a href="https://www.dash.org/forum/"><div class="icon-el dashforum"></div></a>
+							<a href="https://staging-www.dash.org/forum/"><div class="icon-el dashforum"></div></a>
 							<a href="https://blog.dash.org/"><div class="icon-el dashblog"></div></a>
 						</div>
 
@@ -30,7 +30,6 @@
 							<a href="https://twitter.com/Dashpay"><div class="icon-el twitter"></div></a>
 							<a href="https://www.youtube.com/c/DashOrg"><div class="icon-el youtube"></div></a>
 							<a href="https://www.linkedin.com/company/10424093"><div class="icon-el linkedin"></div></a>
-							<a href="https://www.facebook.com/DashPay"><div class="icon-el facebook"></div></a>
 							<a href="https://www.instagram.com/dashpay/"><div class="icon-el instagram"></div></a>
 						</div>
 					</div>
@@ -41,17 +40,9 @@
 
 	<div class="footer-bottom">
 		<div class="container">
-			<div class="brand">
-				<div class="logo">
-					<a href="<?php echo home_url(); ?>">
-						<?php get_template_part('inc/dashlogo.svg'); ?>
-					</a>
-				</div>
-				<p><?php echo get_field('dash_tagline',$homeid); ?></p>
-			</div>
 			<div class="row">
 				
-				<div class="col-lg-7 has-border">
+				<div class="col-lg-7 has-border desktop-footer">
 					<div class="row sitemap">
 
 					<?php while( have_rows('navigation_main',$homeid) ): the_row();
@@ -67,7 +58,7 @@
 								setup_postdata($post);
 								$idx++; ?>
 								<?php if ($idx==1) {?> 
-								<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+								<h4><!--<a href="<?php the_permalink(); ?>">--><?php the_title(); ?><!--</a>--></h4>
 								<?php } else { ?> 
 								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 								<?php }?>
@@ -79,39 +70,50 @@
 						
 					</div>
 				</div>
-				<div class="col-lg-2 has-border">
-					<div class="about">
-
-						<?php 
-						$posts = get_field('navigation_about_links',$homeid);
-						foreach( $posts as $post):
-							setup_postdata($post); ?>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<?php endforeach; 
-						wp_reset_postdata(); ?>
-							<a href="https://insight.dash.org"><?php echo(get_field('block_explorer',$homeid)); ?></a>
-					</div>
-				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-7 mobile-footer">
+    <div class="row sitemap" id="navbar-footer">
+        <?php while ( have_rows('navigation_main', $homeid) ): the_row(); 
+            $navposts = get_sub_field('nav_item_list'); ?>
+            <div class="navbar-container-footer col-lg-3 col-6">
+                <div class="navbar-item">
+                    <div class="link">
+                        <!-- Assume first post should be visible as the main item with an arrow -->
+                        <a href="<?php echo get_permalink($navposts[0]->ID); ?>">
+                            <?php echo get_the_title($navposts[0]->ID); ?>
+                        </a>
+                        <span class="arrow"><img src="/wp-content/uploads/arrow-menu.svg"></span>
+                    </div>
+                    <!-- Check if there are more than one post and create dropdown -->
+                    <?php if ( count($navposts) > 1 ): ?>
+                    <div class="dropdown" style="display:none">
+                        <?php foreach( $navposts as $index => $navpost ): if ($index == 0) continue; // Skip the first post as it's used above ?>
+                            <div class="link">
+                                <a href="<?php echo get_the_permalink($navpost->ID); ?>">
+                                    <?php echo get_the_title($navpost->ID); ?>
+                                </a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    </div>
+</div>
+				<div class="col-lg-3 block-support">
+				<a href="https://support.dash.org"><div class="support">
+				  <div class="i-support">
+				    <img src="https://media.dash.org/wp-content/uploads/support.svg">
+				  </div>
+				  <div class="t-support">
+				  <strong>Contact support</strong>
+				  <p>Full services support desk with humans</p>
+				  </div>
+					</div></a>
 					<div class="row newsletter">
 						<div class="col-12">
 							<?php get_template_part('inc/newsletterform'); ?>
 						</div>
-					</div>
-					<div class="support">
-					<p><?php echo get_field('support_page_text',$homeid);?></p>
-						<a href="<?php echo get_field('support_page_url',$homeid);?>" class="btn btn-ghost">
-							<?php echo get_field('support_page_button',$homeid);?>
-						</a>
-				
-					</div>
-					<div class="status d-flex justify-content-center mt-3">
-						<a href="https://statuspage.freshping.io/1446-Dash">
-							<img src="https://statuspage.freshping.io/badge/6bd13996-45b7-47b0-aaa4-5762412982a7?0.8091951318490984"/>
-						</a>
-					</div>
-					<div class="trustpilot">
-					  <a href="https://www.trustpilot.com/review/www.dash.org" target="_blank" rel="noopener"><img src="/wp-content/themes/dash-theme/assets/img/trustpilot.svg"></a>
 					</div>
 				</div>
 
@@ -119,25 +121,25 @@
 		</div>
 	</div>
 	<div class="footer-terms">
-		<div class="terms container">
+	        <div class="copyright">©2025 Dash. All rights reserved</div>
+		<div class="terms">
 			<?php 
 			$posts = get_field('navigation_privacy',$homeid);
 			$post = $posts[0];
 			setup_postdata($post); 
 			?>
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			<span class="divider">|</span>
+			<span class="divider">/</span>
 			<?php 
 			$post = $posts[1];
 			setup_postdata($post); ?>
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			<span class="divider">|</span>
+			<span class="divider">/</span>
 			<?php 
 			$post = $posts[2];
 			setup_postdata($post); ?>
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			<?php wp_reset_postdata(); ?>	
-
+			<?php wp_reset_postdata(); ?>							
 		</div>
 	</div>
 
