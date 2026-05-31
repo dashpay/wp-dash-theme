b60217a597aef5c91545bf6d2af61a58990acd02
Tanya Namistyuk <tanyanamistyuk@mac.home>
2026-05-31 18:51:33 -0300

    classes-changes-continue

--- inc/blocks/app.php
+++ inc/blocks/app.php
@@ -1,41 +1,45 @@
 <?php
 // Block: App Callout
 // Renders app download section with store links and phone image
-
+// Partial Bootstrap cleanup: layout moved to Dash utilities
 ?>
-<div class="row">
-	<div class="col-lg-6">
 
-		<div class="block-pad-v fade-in-left">
+<div class="dash-app-callout">
+	<div class="dash-app-callout__content fade-in-left">
+		<div class="block-pad-v">
 			<div class="container-xs">
 				<div class="richtext">
-					<?php echo get_sub_field('section_description') ?>
+					<?php echo get_sub_field('section_description'); ?>
 				</div>
 			</div>
 
-			<div class="row d-none d-md-flex py-3">
-
-				<?php
-				$links = get_sub_field('block_list');
-				foreach ( $links as $link) { ?>
-
-					<div class="col-4">
-						<a href="<?php echo $link['block_link'] ?>" class="btn btn-hovershadow">
-							<img src="<?php echo $link['block_item_image']['url'] ?>" alt="<?php echo $link['block_item_image']['alt'] ?>" class="img-fluid">
-							<span><strong><?php echo $link['block_item_title'] ?></strong></span><span><?php echo $link['block_item_description'] ?></span>
-						</a>
-					</div>
-			 	<?php } ?>
-			</div>
+			<?php
+			$links = get_sub_field('block_list');
+			if (is_array($links)) { ?>
+				<div class="dash-app-links">
+					<?php foreach ($links as $link) { ?>
+						<div class="dash-app-links__item">
+							<a href="<?php echo $link['block_link']; ?>" class="btn btn-hovershadow dash-app-link">
+								<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>">
+								<span><strong><?php echo $link['block_item_title']; ?></strong></span>
+								<span><?php echo $link['block_item_description']; ?></span>
+							</a>
+						</div>
+					<?php } ?>
+				</div>
+			<?php } ?>
 
 			<div class="container-xs buttons">
-				<a href="<?php echo get_sub_field('block_link') ?>" class="btn btn-ghost"><?php echo get_sub_field('block_action') ?></a>
+				<a href="<?php echo get_sub_field('block_link'); ?>" class="btn btn-ghost">
+					<?php echo get_sub_field('block_action'); ?>
+				</a>
 			</div>
 		</div>
 	</div>
-	<div class="col-lg-6 col-right">
-		<div class="image image-float bottom right">
-			<img width="450" src="/wp-content/uploads/home-phone-hires.png" class="img-fluid">
+
+	<div class="dash-app-callout__media col-right">
+		<div class="image image-float bottom right dash-app-callout__image">
+			<img width="450" src="/wp-content/uploads/home-phone-hires.png" alt="">
 		</div>
 	</div>
 </div>
