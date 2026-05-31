2c1ac2c928753c895bbff761bd86323493a88056
Tanya Namistyuk <tanyanamistyuk@mac.home>
2026-05-31 18:38:15 -0300

    classes-changes

--- inc/blocks/2col.php
+++ inc/blocks/2col.php
@@ -5,183 +5,204 @@
 // Right column varies by $type
 
 // ACF true/false fields are bugged and always return false. using dropdown values 'yes' and 'no'
+
+$expand = get_sub_field('2_col_expand_column');
+$swap_columns = get_sub_field('2_col_swap_columns') == 'yes';
+
+$layout_class = 'dash-two-col';
+
+switch ($expand) {
+	case 'yes':
+		$layout_class .= ' dash-two-col--wide-media';
+		break;
+	case 'narrow':
+		$layout_class .= ' dash-two-col--wide-content';
+		break;
+	default:
+		$layout_class .= ' dash-two-col--equal';
+		break;
+}
+
+if ($swap_columns) {
+	$layout_class .= ' dash-two-col--swap';
+}
+
+if ($type == '2col_app') {
+	$layout_class .= ' dash-two-col--app';
+}
 ?>
+
 <div class="two-col-title">
-	<?php if ( get_sub_field ( "section_title")!=''){ ?>
+	<?php if (get_sub_field("section_title") != '') { ?>
 		<h3><strong><?php echo get_sub_field('section_title'); ?></strong></h3>
-		<?php } ?>
-		<?php if ( get_sub_field ( "section_subheading")!=''){ ?>
-	<h4><?php echo get_sub_field('section_subheading'); ?></h4>
+	<?php } ?>
+
+	<?php if (get_sub_field("section_subheading") != '') { ?>
+		<h4><?php echo get_sub_field('section_subheading'); ?></h4>
 	<?php } ?>
 </div>
-<div class="row align-items-center">
-	<div class="<?php
-	$expand = get_sub_field('2_col_expand_column');
-	switch($expand) {
-		case 'yes':    echo 'col-lg-4'; break;
-		case 'narrow': echo 'col-lg-7'; break;
-		default:       echo 'col-lg-6'; break;
-	}
-	if ($type=='2col_app') { echo(' block-pad-v'); }
-	?>
-	<?php echo get_sub_field('2_col_swap_columns')=='yes'?'col-right order-lg-2':''; ?>">
 
-		<div class="container-xs <?php echo get_sub_field('2_col_swap_columns')=='yes'?'fade-in-right':'fade-in-left'; ?>">
+<div class="<?php echo $layout_class; ?>">
+	<div class="dash-two-col__content <?php echo $swap_columns ? 'col-right fade-in-right' : 'fade-in-left'; ?>">
+		<div class="container-xs">
 			<div class="richtext">
-			<?php echo get_sub_field('section_description') ?>
+				<?php echo get_sub_field('section_description'); ?>
 			</div>
+
 			<?php
-			 $links = get_sub_field('2_col_links');
-			 if (isset($links[0])){ ?>
-			 	<div class="link-list">
-					<?php foreach ( $links as $link) { ?>
-					<a href="<?php echo $link['link_url']; ?>" target="_blank">
-						<span class="icon-inline sm <?php echo $darkbg?'white':'blue'; ?>">
-							<?php get_template_part('inc/offsite.svg'); ?>
-						</span>
-						<?php echo $link['link_title']; ?>
-					</a>
-			 		<?php } ?>
+			$links = get_sub_field('2_col_links');
+			if (isset($links[0])) { ?>
+				<div class="link-list">
+					<?php foreach ($links as $link) { ?>
+						<a href="<?php echo $link['link_url']; ?>" target="_blank">
+							<span class="icon-inline sm <?php echo $darkbg ? 'white' : 'blue'; ?>">
+								<?php get_template_part('inc/offsite.svg'); ?>
+							</span>
+							<?php echo $link['link_title']; ?>
+						</a>
+					<?php } ?>
 				</div>
-			 <?php } ?>
-			 <?php
-			 $icons = get_sub_field('2_col_icons');
-			 if (isset($icons[0])){ ?>
-			 	<div class="icon-list">
-					<?php foreach ( $icons as $icon) { ?>
+			<?php } ?>
+
+			<?php
+			$icons = get_sub_field('2_col_icons');
+			if (isset($icons[0])) { ?>
+				<div class="icon-list">
+					<?php foreach ($icons as $icon) { ?>
 						<div class="icon-block">
-							<img src="<?php echo $icon['icon_image']['url'] ?>" alt="<?php echo $icon['icon_image']['alt'] ?>" class="img-icons">
+							<img src="<?php echo $icon['icon_image']['url']; ?>" alt="<?php echo $icon['icon_image']['alt']; ?>" class="img-icons">
 							<h6><?php echo $icon['icon_title']; ?></h6>
 						</div>
-			 		<?php } ?>
+					<?php } ?>
 				</div>
-			 <?php } ?>
+			<?php } ?>
 		</div>
+
 		<?php
-		 $links = get_sub_field('block_buttons');
-		 if (isset($links[0])){ ?>
-		 	<div class="container-xs">
-				<div class="pt-md-5 pt-3 buttons">
-					<?php foreach ( $links as $link) {
+		$links = get_sub_field('block_buttons');
+		if (isset($links[0])) { ?>
+			<div class="container-xs">
+				<div class="dash-button-group buttons">
+					<?php foreach ($links as $link) {
 						$class = 'btn-ghost';
-						if ( $darkbg ){ $class .= ' white'; }
-						else { $class .= ' blue'; }
-						if ($link['button_style']=='solid') {
-							if ( $darkbg ){ $class = 'btn-white';}
-							else { $class = 'btn-blue';}
+
+						if ($darkbg) {
+							$class .= ' white';
+						} else {
+							$class .= ' blue';
 						}
-					 ?>
-					<a href="<?php echo $link['button_url']; ?>" class="btn <?php echo $class ?>"><?php echo $link['button_title']; ?></a>
-			 		<?php } ?>
+
+						if ($link['button_style'] == 'solid') {
+							if ($darkbg) {
+								$class = 'btn-white';
+							} else {
+								$class = 'btn-blue';
+							}
+						}
+					?>
+						<a href="<?php echo $link['button_url']; ?>" class="btn <?php echo $class; ?>">
+							<?php echo $link['button_title']; ?>
+						</a>
+					<?php } ?>
 				</div>
 			</div>
-		 <?php } ?>
+		<?php } ?>
 	</div>
-	<?php
-		switch($expand) {
-			case 'yes':    $colclass = 'col-lg-8'; break;
-			case 'narrow': $colclass = 'col-lg-5'; break;
-			default:       $colclass = 'col-lg-6'; break;
-		}
-		if ( get_sub_field('2_col_swap_columns')=='yes' ){
-			$colclass .= ' col-right';
-		}
-	?>
 
-	<?php if ($type=='2col_grid'){ ?>
-		<div class="<?php echo $colclass; ?>">
-			<div class="row text-center grid-item-container">
+	<?php if ($type == '2col_grid') { ?>
+		<div class="dash-two-col__media">
+			<div class="dash-grid-list text-center grid-item-container">
 				<?php
 				$links = get_sub_field('block_list');
 				$subtype = get_sub_field('2_col_grid_style');
-				foreach ( $links as $link) {?>
 
-					<?php if ($subtype=='vendor'){ ?>
-						<div class="col-6">
-							<a <?php if ( $link['block_link']!=''){ ?>href="<?php echo $link['block_link']?>" target="_blank"<?php } ?> class="btn btn-vendor btn-white">
+				foreach ($links as $link) { ?>
+
+					<?php if ($subtype == 'vendor') { ?>
+						<div class="dash-grid-list__item dash-grid-list__item--half">
+							<a <?php if ($link['block_link'] != '') { ?>href="<?php echo $link['block_link']; ?>" target="_blank"<?php } ?> class="btn btn-vendor btn-white">
 								<div class="icon">
-								<?php echo $link['block_item_image']?'<img src="'.$link['block_item_image'].'" alt>':'<span class="icon-placeholder"></span>' ?>
+									<?php echo $link['block_item_image'] ? '<img src="' . $link['block_item_image'] . '" alt>' : '<span class="icon-placeholder"></span>'; ?>
 								</div>
 								<div class="block-icons">
 									<div class="vendor-title-desc">
-										<span class="title"><?php echo $link['block_item_title'] ?></span>
-										<span class="link"><?php echo $link['block_item_description'] ?></span>
-					                </div>
-									<div class="vendor-button">
-										<span class="button"><?php echo $link['callout_block_link_text'] ?></span>
-					                </div>
-								</div>
-							</a>
-						</div>
-					<?php }?>
-
-
-					<?php if ($subtype=='social'){ ?>
-						<div class="col-lg-3 col-6">
-							<a <?php if ( $link['block_link']!=''){ ?>href="<?php echo $link['block_link']?>" target="_blank"<?php } ?> class="btn btn-hover">
-								<div class="image">
-									<img src="<?php echo $link['block_item_image']['url'] ?>" alt="<?php echo $link['block_item_image']['alt'] ?>" class="img-fluid socialicons">
-								</div>
-								<?php if ( $link['block_item_title']!=''){ ?>
-									<span><?php echo $link['block_item_title']?></span>
-								<?php } ?>
-								<?php if ( $link['block_item_description']!=''){ ?>
-									<small><?php echo $link['block_item_description'] ?></small>
-								<?php } ?>
-							</a>
-						</div>
-					<?php }?>
-
-
-					<?php if ($subtype=='default'){ ?>
-						<div class="col-lg-4 col-6">
-							<div class="grid-item">
-									<div class="image">
-										<?php if ( $link['block_link']!=''){ ?>
-										<a href="<?php echo $link['block_link']?>" target="<?php if ( get_sub_field( "grid_link_open_new_tab" ) ) { echo "_blank"; }?>">
-											<img src="<?php echo $link['block_item_image']['url'] ?>" alt="<?php echo $link['block_item_image']['alt'] ?>" class="img-fluid">
-										</a>
-										<?php } else { ?>
-											<img src="<?php echo $link['block_item_image']['url'] ?>" alt="<?php echo $link['block_item_image']['alt'] ?>" class="img-fluid">
-										<?php } ?>
+										<span class="title"><?php echo $link['block_item_title']; ?></span>
+										<span class="link"><?php echo $link['block_item_description']; ?></span>
 									</div>
-								<?php if ( $link['block_item_title']!=''){ ?>
-									<p><?php echo $link['block_item_title']?></p>
+									<div class="vendor-button">
+										<span class="button"><?php echo $link['callout_block_link_text']; ?></span>
+									</div>
+								</div>
+							</a>
+						</div>
+					<?php } ?>
+
+					<?php if ($subtype == 'social') { ?>
+						<div class="dash-grid-list__item dash-grid-list__item--quarter">
+							<a <?php if ($link['block_link'] != '') { ?>href="<?php echo $link['block_link']; ?>" target="_blank"<?php } ?> class="btn btn-hover">
+								<div class="image">
+									<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>" class="img-fluid socialicons">
+								</div>
+								<?php if ($link['block_item_title'] != '') { ?>
+									<span><?php echo $link['block_item_title']; ?></span>
+								<?php } ?>
+								<?php if ($link['block_item_description'] != '') { ?>
+									<small><?php echo $link['block_item_description']; ?></small>
+								<?php } ?>
+							</a>
+						</div>
+					<?php } ?>
+
+					<?php if ($subtype == 'default') { ?>
+						<div class="dash-grid-list__item dash-grid-list__item--third">
+							<div class="grid-item">
+								<div class="image">
+									<?php if ($link['block_link'] != '') { ?>
+										<a href="<?php echo $link['block_link']; ?>" target="<?php if (get_sub_field("grid_link_open_new_tab")) { echo "_blank"; } ?>">
+											<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>" class="img-fluid">
+										</a>
+									<?php } else { ?>
+										<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>" class="img-fluid">
+									<?php } ?>
+								</div>
+
+								<?php if ($link['block_item_title'] != '') { ?>
+									<p><?php echo $link['block_item_title']; ?></p>
 								<?php } ?>
 
-								<?php if ( $link['block_item_description']!=''){ ?>
-									<small><?php echo $link['block_item_description'] ?></small>
+								<?php if ($link['block_item_description'] != '') { ?>
+									<small><?php echo $link['block_item_description']; ?></small>
 								<?php } ?>
 							</div>
 						</div>
-					<?php }?>
+					<?php } ?>
 
-					<?php if ($subtype=='feature'){ ?>
-						<div class="col-lg-6 col-12">
-							<a <?php if ( $link['block_link']!=''){ ?>href="<?php echo $link['block_link']?>" target="_blank"<?php } ?> class="btn btn-hover feature-panel">
+					<?php if ($subtype == 'feature') { ?>
+						<div class="dash-grid-list__item dash-grid-list__item--half">
+							<a <?php if ($link['block_link'] != '') { ?>href="<?php echo $link['block_link']; ?>" target="_blank"<?php } ?> class="btn btn-hover feature-panel">
 								<div class="image">
-									<img src="<?php echo $link['block_item_image']['url'] ?>" alt="<?php echo $link['block_item_image']['alt'] ?>" class="img-fluid socialicons">
+									<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>" class="img-fluid socialicons">
 								</div>
-								<?php if ( $link['block_item_title']!=''){ ?>
-									<span><?php echo $link['block_item_title']?></span>
+								<?php if ($link['block_item_title'] != '') { ?>
+									<span><?php echo $link['block_item_title']; ?></span>
 								<?php } ?>
-								<?php if ( $link['block_item_description']!=''){ ?>
-									<small><?php echo $link['block_item_description'] ?></small>
+								<?php if ($link['block_item_description'] != '') { ?>
+									<small><?php echo $link['block_item_description']; ?></small>
 								<?php } ?>
 							</a>
 						</div>
-					<?php }?>
+					<?php } ?>
 
-			 	<?php } ?>
+				<?php } ?>
 			</div>
 		</div>
 	<?php } ?>
 
-	<?php if ($type=='2col_image'){ ?>
-		<div class="<?php echo $colclass; ?> d-lg-block d-none">
+	<?php if ($type == '2col_image') { ?>
+		<div class="dash-two-col__media dash-hide-mobile">
 			<div class="image">
-				<?php if ( get_sub_field('2_col_large_image')!='' ) { ?>
-					<img src="<?php echo get_sub_field('2_col_large_image') ?>" alt class="img-fluid">
+				<?php if (get_sub_field('2_col_large_image') != '') { ?>
+					<img src="<?php echo get_sub_field('2_col_large_image'); ?>" alt class="img-fluid">
 				<?php } else {
 					echo get_sub_field('2_col_large_video');
 				} ?>
@@ -189,45 +210,45 @@
 		</div>
 	<?php } ?>
 
-	<?php if ($type=='2col_app'){ ?>
-		<div class="<?php echo $colclass; ?> block-pad-top">
+	<?php if ($type == '2col_app') { ?>
+		<div class="dash-two-col__media block-pad-top">
 			<div class="image">
-				<?php if ( get_sub_field('2_col_large_image')!='' ) { ?>
-					<img src="<?php echo get_sub_field('2_col_large_image') ?>" alt class="img-fluid">
+				<?php if (get_sub_field('2_col_large_image') != '') { ?>
+					<img src="<?php echo get_sub_field('2_col_large_image'); ?>" alt class="img-fluid">
 				<?php } ?>
 			</div>
 		</div>
 	<?php } ?>
 
-	<?php if ($type=='2col_terminal'){ ?>
-		<div class="<?php echo $colclass; ?>">
+	<?php if ($type == '2col_terminal') { ?>
+		<div class="dash-two-col__media">
 			<div class="terminal-item">
 				<div class="terminal-wrapper">
 					<div class="terminal-header">
-						<div class="row align-items-center">
-							<div class="col-6">
+						<div class="dash-terminal-header">
+							<div>
 								<i class="dot"></i>
 								<i class="dot"></i>
 								<i class="dot"></i>
 							</div>
-							<div class="col-6 text-right">
-								<a href="#" class="btn btn-download copy-trigger" data-target="#copyarea-<?php echo $count ?>">
+							<div class="dash-terminal-actions">
+								<a href="#" class="btn btn-download copy-trigger" data-target="#copyarea-<?php echo $count; ?>">
 									<i class="icon-el clipboard"></i>
 								</a>
 							</div>
 						</div>
 					</div>
+
 					<div class="terminal-body">
 						<div class="textarea form-control">
-							<?php echo get_sub_field('terminal_text') ?>
+							<?php echo get_sub_field('terminal_text'); ?>
 						</div>
-							<textarea  id="copyarea-<?php echo $count ?>">
-							<?php echo trim(strip_tags(get_sub_field('terminal_text'))) ?>
-							</textarea>
+						<textarea id="copyarea-<?php echo $count; ?>">
+							<?php echo trim(strip_tags(get_sub_field('terminal_text'))); ?>
+						</textarea>
 					</div>
 				</div>
 			</div>
 		</div>
 	<?php } ?>
-
 </div>
--- inc/blocks/callouts.php
+++ inc/blocks/callouts.php
@@ -1,74 +1,94 @@
 <?php
 // Block: Callouts
-// Renders card-based callout blocks with optional large image, buttons
+// Renders card-based callout blocks with optional large image and buttons
+// Partial Bootstrap cleanup: layout/card structure moved to Dash utilities
 
 $largeimg = false;
-if ( get_sub_field('callout_desktop_only_image')!='' ) {
+
+if (get_sub_field('callout_desktop_only_image') != '') {
 	$largeimg = get_sub_field('callout_desktop_only_image');
 }
-if ( $largeimg ){ ?>
-    <div class="image d-none d-lg-block"><img src="<?php echo $largeimg;?>" alt class="large img-fluid"></div>
-<?php }  ?>
 
-<div class="callouts-title">
-	<?php if ( get_sub_field ( "section_title")!=''){ ?>
+if ($largeimg) { ?>
+	<div class="dash-callouts__desktop-image">
+		<img src="<?php echo $largeimg; ?>" alt class="large">
+	</div>
+<?php } ?>
+
+<div class="callouts-title dash-callouts__title">
+	<?php if (get_sub_field("section_title") != '') { ?>
 		<h3><strong><?php echo get_sub_field('section_title'); ?></strong></h3>
-		<?php } ?>
-		<?php if ( get_sub_field ( "section_subheading")!=''){ ?>
-	<h4><?php echo get_sub_field('section_subheading'); ?></h4>
+	<?php } ?>
+
+	<?php if (get_sub_field("section_subheading") != '') { ?>
+		<h4><?php echo get_sub_field('section_subheading'); ?></h4>
 	<?php } ?>
 </div>
+
 <?php
 $links = get_sub_field('block_list');
-$class = "card border-0 text-center bg-transparent";
+$card_class = 'dash-callout-card';
 ?>
 
 <?php if (is_array($links)) : ?>
-<div class="card-deck">
-	<?php foreach ($links as $link) : ?>
-		<div class="<?php echo $class; ?>">
+	<div class="dash-callouts-grid">
+		<?php foreach ($links as $link) : ?>
+			<div class="<?php echo $card_class; ?>">
 
-			<?php if ($largeimg) : ?>
-				<div class="d-lg-none">
-					<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>" class="img-fluid">
-				</div>
-				<div class="card-body">
-					<h3 class="title-small card-title"><?php echo $link['block_item_title']; ?></h3>
-			<?php else : ?>
-				<div class="callout-image">
-					<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>" class="card-img-top">
-				</div>
-				<div class="card-body px-2">
-					<h3 class="title-italic card-title"><?php echo $link['block_item_title']; ?></h3>
-			<?php endif; ?>
+				<?php if ($largeimg) : ?>
+					<div class="dash-callout-card__mobile-image">
+						<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>">
+					</div>
 
-					<p class="card-text"><?php echo $link['block_item_description']; ?></p>
-				</div>
+					<div class="dash-callout-card__body">
+						<h3 class="title-small dash-callout-card__title">
+							<?php echo $link['block_item_title']; ?>
+						</h3>
+				<?php else : ?>
+					<div class="callout-image dash-callout-card__image">
+						<img src="<?php echo $link['block_item_image']['url']; ?>" alt="<?php echo $link['block_item_image']['alt']; ?>">
+					</div>
 
-			<?php if ($link['block_link'] != '') : ?>
-				<div class="card-footer bg-transparent border-0">
-					<a href="<?php echo $link['block_link']; ?>" target="<?php echo get_sub_field('callout_open_new_tab') ? '_blank' : '_self'; ?>" class="btn btn-ghost <?php echo get_field('background_style') == 'bg-gradient-h' ? 'white' : 'blue'; ?>">
-						<strong><?php echo $link['callout_block_link_text'] ?: __('Read more', 'html5blank'); ?></strong>
-					</a>
-				</div>
-			<?php endif; ?>
+					<div class="dash-callout-card__body">
+						<h3 class="title-italic dash-callout-card__title">
+							<?php echo $link['block_item_title']; ?>
+						</h3>
+				<?php endif; ?>
 
-		</div>
-	<?php endforeach; ?>
-</div>
-<?php endif; ?>
-			<div class="container-xs">
-				<div class="pt-md-5 pt-3 buttons">
-					<?php
-					$links = get_sub_field('block_buttons');
-					if (isset($links[0])){ ?>
-						<?php foreach ( $links as $link) {
-							$class = 'btn-ghost blue';
-							if ($link['button_style']=='solid') {
-								$class = 'btn-blue';
-							}
-						?>
-						<a href="<?php echo $link['button_url']; ?>" class="btn <?php echo $class ?>"><?php echo $link['button_title']; ?></a><?php } ?>
-					<?php } ?>
-				</div>
+						<p class="dash-callout-card__text">
+							<?php echo $link['block_item_description']; ?>
+						</p>
+					</div>
+
+				<?php if ($link['block_link'] != '') : ?>
+					<div class="dash-callout-card__footer">
+						<a href="<?php echo $link['block_link']; ?>" target="<?php echo get_sub_field('callout_open_new_tab') ? '_blank' : '_self'; ?>" class="btn btn-ghost <?php echo get_field('background_style') == 'bg-gradient-h' ? 'white' : 'blue'; ?>">
+							<strong><?php echo $link['callout_block_link_text'] ?: __('Read more', 'html5blank'); ?></strong>
+						</a>
+					</div>
+				<?php endif; ?>
+
 			</div>
+		<?php endforeach; ?>
+	</div>
+<?php endif; ?>
+
+<?php
+$buttons = get_sub_field('block_buttons');
+if (isset($buttons[0])) { ?>
+	<div class="container-xs">
+		<div class="dash-callouts__buttons buttons">
+			<?php foreach ($buttons as $link) {
+				$class = 'btn-ghost blue';
+
+				if ($link['button_style'] == 'solid') {
+					$class = 'btn-blue';
+				}
+			?>
+				<a href="<?php echo $link['button_url']; ?>" class="btn <?php echo $class; ?>">
+					<?php echo $link['button_title']; ?>
+				</a>
+			<?php } ?>
+		</div>
+	</div>
+<?php } ?>
